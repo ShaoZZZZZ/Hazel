c9efbed750c7f136f0f3de10e383ca18571d487f
Wu Zheng <OldWuMan@163.com>
2023-07-17 14:04:55 +0800

    Log module
    导入日志，spdlog

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,10 +1,10 @@
 #pragma once
 
 //For use by Hazel applications
-
+#include "Hazel/Log.h"
 #include "Hazel/Application.h"
 #include <stdio.h>
 //---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
-#include "Hazel/Log.h"
+
 //-----------------------------------
